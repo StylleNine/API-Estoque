b29ef7fad0aa9e4072a274171716f7a9889f1413
Style <felipe.alabarce@outlook.com>
2024-08-12 23:24:48 -0300

    fix: adjusting docker to run applicattion factory

--- application/app.py
+++ application/app.py
@@ -1,20 +1,9 @@
-from flask import Flask, jsonify
-from flask_restful import Resource, Api, reqparse
+from flask import jsonify
+from flask_restful import Resource, reqparse
 from mongoengine import NotUniqueError
 from .model import UserModel
 import re
 
-
-app = Flask(__name__)
-
-
-app.config['MONGODB_SETTINGS'] = {
-    'db': 'users',
-    'host': 'mongodb',
-    'port': 27017,
-    'username': 'admin',
-    'password': 'admin'
-}
 
 _user_parser = reqparse.RequestParser()
 _user_parser.add_argument('first_name',
@@ -42,9 +31,6 @@ _user_parser.add_argument('birth_date',
                           required=True,
                           help="This field cannot be blank."
                           )
-
-
-api = Api(app)
 
 
 class Users(Resource):
@@ -99,10 +85,3 @@ class User(Resource):
             return jsonify(response)
         return {"message": "User does not exist in database"}, 400
 
-
-api.add_resource(Users, '/users')
-api.add_resource(User, '/user', '/user/<string:cpf>')
-
-
-if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
