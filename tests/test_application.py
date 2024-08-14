b1788659c4e77d5d7762176e84765fa111960cfb
Style <felipe.alabarce@outlook.com>
2024-08-14 20:51:26 -0300

    test: testing for get user

--- tests/test_application.py
+++ tests/test_application.py
@@ -9,6 +9,51 @@ class TestApplication():
         app = create_app('config.MockConfig')
         yield app.test_client()
 
+    @pytest.fixture
+    def valid_user(self):
+        return {
+            "first_name": "Mateus",
+            "last_name": "Muller",
+            "cpf": "006.730.580-69",
+            "email": "contato@email.com",
+            "birth_date": "1994-09-10"
+        }
+
+    @pytest.fixture
+    def invalid_user(self):
+        return {
+            "first_name": "Mateus",
+            "last_name": "Muller",
+            "cpf": "006.730.580-67",
+            "email": "contato@email.com",
+            "birth_date": "1994-09-10"
+        }
+
     def test_get_users(self, client):
         response = client.get('/users')
         assert response.status_code == 200
+
+    def test_post_user(self, client, valid_user, invalid_user):
+        response = client.post('/user', json=valid_user)
+        assert response.status_code == 200
+        assert b"successfully" in response.data
+
+        response = client.post('/user', json=invalid_user)
+        assert response.status_code == 400
+        assert b"invalid" in response.data
+
+    def test_get_user(self, client, valid_user, invalid_user):
+        response = client.get('/user/%s' % valid_user["cpf"])
+        assert response.status_code == 200
+        assert response.json[0]["first_name"] == "Mateus"
+        assert response.json[0]["last_name"] == "Muller"
+        assert response.json[0]["cpf"] == "006.730.580-69"
+        assert response.json[0]["email"] == "contato@email.com"
+
+        birth_date = response.json[0]["birth_date"]["$date"]
+        assert birth_date == "1994-09-10T00:00:00Z"
+
+        response = client.get('/user/%s' % invalid_user["cpf"])
+        assert response.status_code == 400
+        assert b"User does not exist in database!" in response.data
+ 
