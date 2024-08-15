f616c62bf8da0d38aa1792a395a793ec9b94576c
Style <felipe.alabarce@outlook.com>
2024-08-14 22:11:19 -0300

    test: second test of renaming branch

--- tests/test_application.py
+++ tests/test_application.py
@@ -56,4 +56,4 @@ class TestApplication():
         response = client.get('/user/%s' % invalid_user["cpf"])
         assert response.status_code == 400
         assert b"User does not exist in database" in response.data
-        #alteracao para teste de branch
+        #print('alteracao para teste de branch')
